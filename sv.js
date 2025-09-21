dd8d108409485e32704c5ec4ea7fda585cc8f9d5
sscor77 <109048106+sscor77@users.noreply.github.com>
2025-09-21 13:42:54 +0200

    Create sv.js

---
+++ sv.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "presupuesto-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+// Instalar y guardar en caché
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activar y limpiar caches antiguos
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Responder peticiones con cache primero
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
